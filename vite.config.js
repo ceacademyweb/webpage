893c176ad5b6623b0397215aad2439ae9aa1353a
ceacademyweb <122063649+ceacademyweb@users.noreply.github.com>
2023-02-21 02:35:46 +0000

    652edcd01ff0dfaf8e10a4d25bdc11fb.txt

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,10 @@ export default defineConfig({
           src: '_redirects',
           dest: '',
         },
+        {
+          src: '652edcd01ff0dfaf8e10a4d25bdc11fb.txt',
+          dest: '',
+        },
       ],
     }),
   ],
