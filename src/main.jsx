d115a5462f5749bfaac70c1fe1ee47d311049edb
ceacademyweb <ceacademy.web@gmail.com>
2023-06-13 20:18:45 -0600

    new home

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Blank from './pages/Blank';
 import './scss/styles.scss';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from './queryClient';
@@ -10,7 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        {/* <App /> */}
+        <Blank />
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
